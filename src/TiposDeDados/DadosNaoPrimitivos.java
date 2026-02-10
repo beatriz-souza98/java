f462bbbd48884bcfacf5fd8b6db89454d6c8a58e
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-02-10 00:03:28 -0300

    Initial commit

---
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,15 @@
+package TiposDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Naruto";
+        String nomeEmCaixaAlta = nome.toUpperCase();
+        System.out.println("Esse texto está em CapsLock " + nomeEmCaixaAlta);
+
+        String aldeia = "Aldeia da Folha";
+        String aldeiaEmCaixaBaixa = aldeia.toLowerCase();
+        System.out.println("Esse texto está em minusculo " + aldeiaEmCaixaBaixa);
+
+
+    }
+}
---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,12 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /* Criar um ninja */
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 9999999L;
+    }
+}
