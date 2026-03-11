bdc85a4d410e629f74e45c87f8ceb0ebbbc012de
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-03-10 23:14:25 -0300

    Classes

---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,21 @@
+package NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Criar o ninja Naruto
+
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.idade = 18;
+        Naruto.aldeia = "Aldeia da Folha";
+
+        // Criar ninja Sasuke
+
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.idade = 17;
+        Sasuke.aldeia = "Aldeia da Folha";
+
+    }
+}
