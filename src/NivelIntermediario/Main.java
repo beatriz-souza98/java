f79a4fdfe82cae5fd7aa0dd1441264856769fbb0
beatriz-souza98 <beatrizsouza231998@gmail.com>
2026-03-10 23:54:36 -0300

    Adicionando Metodos

--- src/NivelIntermediario/Main.java
+++ src/NivelIntermediario/Main.java
@@ -10,12 +10,25 @@ public class Main {
         Naruto.idade = 18;
         Naruto.aldeia = "Aldeia da Folha";
 
+        //Aplicando metodos aos meus objetos
+
+        Naruto.SharinganAtivado();
+
         // Criar ninja Sasuke
 
         Ninja Sasuke = new Ninja();
         Sasuke.nome = "Sasuke Uchiha";
         Sasuke.idade = 17;
         Sasuke.aldeia = "Aldeia da Folha";
+
+        // Fazer metodo String que retorna uma String
+
+        String chamandoMetodo = Sasuke.boasVindasMeuNomeE();
+        System.out.println(chamandoMetodo);
+
+        //Fazer metodo int que mostra um int
+        int idadeQueFalta = Sasuke.anosParaSerHohake(25);
+        System.out.println("Ainda falta " + idadeQueFalta + " anos para você se tornar um Hohake");
 
     }
 }
--- src/NivelIntermediario/Ninja.java
+++ src/NivelIntermediario/Ninja.java
@@ -6,4 +6,17 @@ public class Ninja {
     String aldeia;
     int idade;
 
+    //Criar um metodo público personalizado
+    public void SharinganAtivado(){
+        System.out.println("O Sharingan ativou!");
+    }
+
+    public String boasVindasMeuNomeE(){
+        return "Oi, eu sou um ninja!";
+    }
+
+    public int anosParaSerHohake(int idadeMinina){
+        return idadeMinina - idade;
+    }
+
     }
